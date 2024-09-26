07ea2513fc0567f76b630baf447383450b412ddf
João Vitor Sousa <joaovfsousa@gmail.com>
2024-09-25 21:45:54 -0300

    feat: disable logs by default

--- internal/logger.go
+++ internal/logger.go
@@ -1,7 +1,22 @@
 package logger
 
-import "log/slog"
+import (
+	"log/slog"
+	"os"
+)
+
+type NullWriter struct{}
+
+func (NullWriter) Write([]byte) (int, error) { return 0, nil }
+
+func NullLogger() *slog.Logger {
+	return slog.New(slog.NewTextHandler(NullWriter{}, nil))
+}
 
 func GetLogger() *slog.Logger {
-	return slog.New(slog.Default().Handler())
+	if os.Getenv("DEBUG") == "1" {
+		return slog.New(slog.Default().Handler())
+	}
+
+	return NullLogger()
 }
