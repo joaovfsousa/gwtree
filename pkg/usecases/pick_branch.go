5adf599f45f0f9c594f23c9bffa54d95057ce3e7
João Vitor Sousa <joaovfsousa@gmail.com>
2024-10-01 14:34:11 -0300

    fix: better message when cancel fzf

--- pkg/usecases/pick_branch.go
+++ pkg/usecases/pick_branch.go
@@ -1,13 +1,15 @@
 package usecases
 
 import (
+	"errors"
+
 	"github.com/joaovfsousa/gwtree/internal/os_commands"
 )
 
 func (uc *UseCases) PickBranch() (string, error) {
 	branches, err := uc.gc.Branch.ListBranches()
 	if err != nil {
-		return "", nil
+		return "", errors.New("Failed to select a branch")
 	}
 
 	branch_names := []string{}
@@ -17,7 +19,7 @@ func (uc *UseCases) PickBranch() (string, error) {
 
 	selectedBranch, err := os_commands.FzfSelect(branch_names)
 	if err != nil {
-		return "", nil
+		return "", errors.New("Failed to select a branch")
 	}
 
 	return selectedBranch, nil
